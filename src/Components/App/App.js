1778d23db9d24bca6e49f81eba65090320f8fce5
Kramarus <rkramarev@gmail.com>
2022-09-06 14:33:06 +0200

    url state fixed

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Landing from "../Landing/Landing";
 
@@ -7,15 +7,16 @@ export default function App(props) {
   return (
     <>
  
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Landing lang="en" />} />
           <Route path="/en" element={<Landing lang="en" />} />
           <Route path="/de" element={<Landing lang="de" />} />
           <Route path="/ua" element={<Landing lang="ua" />} />
           <Route path="/ru" element={<Landing lang="ru" />} />
+          {/* <Route path="/*" element={<Page404 lang="en" />} /> */}
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
